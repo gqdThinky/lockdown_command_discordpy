4da24da5d1908984e7e555d1dbd24e0a527e7b38
gqdThinky <104022323+gqdThinky@users.noreply.github.com>
2022-04-19 21:52:53 +0200

    Create main.py
    The main file of the code.

---
+++ main.py
@@ -0,0 +1,31 @@
+# BY @gqdThinky 
+# YouTube : https://youtube.com/c/obvThinky 
+
+
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix = ";")
+
+@bot.event
+async def on_ready():
+	print("xxxxxx")
+
+
+
+@bot.command()
+@commands.has_permissions(manage_channels=True)
+async def lockdown(ctx):
+    await ctx.channel.set_permissions(ctx.guild.default_role,send_messages=False)
+    await ctx.send(ctx.channel.mention + " ***is now in lockdown***")
+    
+@bot.command()
+@commands.has_permissions(manage_channels=True)
+async def unlock(ctx):
+    await ctx.channel.set_permissions(ctx.guild.default_role,send_messages=True)
+    await ctx.send(ctx.channel.mention + " ***has been unlocked.***")
+
+
+
+
+bot.run("TOKEN")
